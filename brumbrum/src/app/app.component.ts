47c521b941fcf95bc67fa815593896eefb82bebc
Corina Perez Figuera <korypf@gmail.com>
2024-04-16 17:46:09 +0100

    stylish components

--- brumbrum/src/app/app.component.ts
+++ brumbrum/src/app/app.component.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from "./pages/home/home.component";
-// import { BrowserModule } from '@angular/platform-browser';
-// import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 @Component({
